b642e63062c81c95042d541c2f30b5c2b12570a8
JahidFahad <jahidfahad017@gmail.com>
2020-03-04 09:44:39 +0600

    true vs false js

---
+++ true-false.js
@@ -0,0 +1,13 @@
+// name = '';
+// name= 0;
+// name= NaN;
+// name= null;
+// name;
+
+
+const name = '';
+if(name){
+    console.log("Condition is true.")
+}else{
+    console.log("Condition is false")
+}
